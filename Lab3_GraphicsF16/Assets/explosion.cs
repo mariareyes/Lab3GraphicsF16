0318cba68e14170f42becec00ad0bec4d800e194
Maria <mariafreaner@gmail.com>
2016-12-03 21:54:51 -0500

    Lab3 for Graphics

---
+++ Lab3_GraphicsF16/Assets/explosion.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explosion : MonoBehaviour {
+    public ParticleSystem exp;
+    public void OnCollisionEnter(Collision collision)
+    {
+         exp = GetComponentInChildren<ParticleSystem>();
+        exp.Play();
+        Destroy(gameObject);
+    }
+}
---
+++ Lab3_GraphicsF16/Assets/explosion2.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class explosion2 : MonoBehaviour {
+
+    public ParticleSystem exp;
+    public void OnCollisionEnter(Collision collision)
+    {
+        exp = GetComponentInChildren<ParticleSystem>();
+        exp.Play();
+        Destroy(gameObject);
+    }
+}
